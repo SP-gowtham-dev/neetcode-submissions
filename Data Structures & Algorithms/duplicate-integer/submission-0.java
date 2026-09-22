e6d8503e08a742a914a3645cbb4b7acb10d101f6
SP-gowtham-dev <gothamsenthilkumar@gmail.com>
2026-09-22 08:43:00 -0400

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+
+        boolean isTrue = false;
+        
+        for (int i = 0; i < nums.length;i++){
+            for (int j = 0; j < nums.length; j++){
+                if ((nums[i] == nums[j])&&(i!=j)){
+                    isTrue =  true;
+                }
+            }
+        }
+
+        return isTrue;
+        
+    }
+}
